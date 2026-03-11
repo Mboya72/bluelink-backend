f86657b5a6618a8110c7a9f7177229253cb2e40e
Mboya72 <elvindio72@gmail.com>
2026-03-11 17:49:55 +0300

    adding ads models

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Advertisement(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+
+    image = models.ImageField(upload_to="ads/")
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    approved = models.BooleanField(default=False)
