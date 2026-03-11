b567ebd0ffb31ea6fa4a320907e822e8dc90e568
Mboya72 <elvindio72@gmail.com>
2026-03-11 17:51:04 +0300

    adding analytics models

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class UserActivity(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    activity_type = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductView(models.Model):
+
+    product_id = models.IntegerField()
+
+    viewer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    viewed_at = models.DateTimeField(auto_now_add=True)
